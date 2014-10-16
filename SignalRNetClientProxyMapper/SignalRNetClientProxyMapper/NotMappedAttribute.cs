76c37974b61c456c29a8db5ad7c81003ec9dc5fe
Oliver C. Baker <oliver.baker@sixnetworks.net>
2014-10-16 11:35:33 +0200

    All new code now complies with Code Analysis

--- SignalRNetClientProxyMapper/SignalRNetClientProxyMapper/NotMappedAttribute.cs
+++ SignalRNetClientProxyMapper/SignalRNetClientProxyMapper/NotMappedAttribute.cs
@@ -5,5 +5,6 @@ namespace SignalRNetClientProxyMapper
     /// <summary>
     ///     Disables Mapping of a function for Strongly Typed Hubs
     /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public sealed class NotMappedAttribute : Attribute { }
 }
